5eabf3ac45b1ec4d0945e0941bf5600022cc0d12
rahul241172 <rahulgandhi241172@gmail.com>
2022-10-19 16:04:05 +0530

    erroe

---
+++ problem1.js
@@ -0,0 +1,8 @@
+let num=13
+let factors=0
+
+for(let i=1;i<=num;i++){
+    if(num%i==1){
+        console.log("true")
+    }
+}
---
+++ problem2.js
@@ -0,0 +1,6 @@
+let str="racecar"
+bag=""
+for(let i=str.length;i>0;i--){
+    bag+=str[i]
+}
+console.log(bag)
