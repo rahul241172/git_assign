f7778428cfb7f6cf4fdd6bedc80ed75c2eacb57b
kumkum19 <kumkumdutta.matelli@gmail.com>
2022-10-19 16:16:59 +0530

    I have corrected

--- problem1.js
+++ problem1.js
@@ -2,7 +2,11 @@ let num=13
 let factors=0
 
 for(let i=1;i<=num;i++){
-    if(num%i==1){
-        console.log("true")
-    }
+    if(num%i==0){
+factors++
+        }
 }
+if(factors==2){
+console.log("Prime)
+}else{
+console.log("Not Prime")
